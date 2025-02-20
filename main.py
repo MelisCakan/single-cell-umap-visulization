1f473fa03ad1407564b4ed39a2ef1623ab2ae407
MelisCakan <cakanmelis27@gmail.com>
2025-02-20 17:20:09 +0300

    app_ui mistake has been fixed

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import scanpy as sc
 import matplotlib.pyplot as plt
-from shiny import reactive, App
-from shiny.express import ui, render, input
+from shiny import reactive, App, ui
+from shiny.express import render, input
 import os
 import zipfile
 import functions as f
 
-app_ui = ui.page(
-    ui.tags.h1("UMAP Visulization Tool", class_ = "title"),
-    ui.tags.label("Your zip file must contain a h5ad file to create an umap."),
+app_ui = ui.page_fluid(
+    ui.h1("UMAP Visulization Tool", class_ = "title"),
+    ui.p("Your zip file must contain a h5ad file to create an umap."),
     ui.input_file("file_input", "Upload a zip file.", accept=".zip"),
     ui.output_text("output_message"),
     ui.output_image("output_image")
